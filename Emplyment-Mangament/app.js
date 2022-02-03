774f7dd296d1c626d0c976bc4e65895e99679570
Michael Rawlings <rawlingsm99@gmail.com>
2022-02-02 23:00:27 -0600

    added a bunch of code to app.js

--- Emplyment-Mangament/app.js
+++ Emplyment-Mangament/app.js
@@ -50,6 +50,120 @@ const mainOption =() => {
   }
 
 
+// view functioon
+const viewFunction = () => {
+    inquirer
+      .prompt({
+        name: 'view',
+        type: 'list',
+        message: 'What data would you like to view?',
+        Choices ['Department', 'Role', 'Employee' ,'View employee by manager' ,'View department budget' ,'Back']
+      })
+
+      .then((chosen) => {
+        switch(chosen.view) {
+          case 'Department':
+            viewDepartment();
+            break;
+            case 'Role':
+              viewRole();
+            break;
+              case 'Employee':
+                viewEmployee();
+            break;
+              case 'View employee by manager':
+                chooseManager();
+            break;
+              case 'View department budget':
+            break;
+            default:
+              mainOption();
+        }
+      });
+}
+
+const viewDepartment = ()=> {
+  db.query(selectDepartment, function(err, res) {
+    if(err) throw err;
+    console.table(res);
+    mainOption();
+});
+}
+
+const viewRole =() => {
+  db.query(selectRole, function(err, res) {
+  if(err) throw err;
+  console.table(res);
+  mainOption();
+});
+}
+
+
+const viewEmployee =() => {
+  db.query(selectEmployee, function(err, res) {
+    if(err) throw err;
+    console.table(res);
+    mainOption();
+});
+}
+
+const chooseManager = ()=> {
+  db.query(selectIdandNameFromEmployee,
+     function(err, res) {
+      if(err) throw err;
+       inquirer
+        .prompt({
+          name: 'Manager',
+          type: 'rawlist'
+          message: 'choose manager'
+            choices: () => {
+              const list = [];
+              for(let i = 0; i < res.length; i++) {
+                list.push(res[i]);
+              }
+              return list;
+            }
+        })
+        .then((chosen) => {
+          const managerId = findId(chosen.manager, res);
+          db.query(`SELECT id FROM employee WHERE id = ?`, managerId, Function(err, res) { if (err) throw err;
+            viewEmployeeByManager(res);
+          });
+        });
+  });
+}
+  
+}
+const viewEmployeeByManager = manager) => {
+  db.query(`
+  SELCT
+      employee.id AS 'ID',
+      employee.first_name AS 'First Name',
+      department.name AS 'Department',
+      role.title AS 'Role',
+      role.salary AS 'Salary'
+  FROM department
+  INNER JOIN role ON departent.id = role.department_id
+  INNER HOIN employee ON role.id = employee.role_id
+  WHERE mangage_id =?;
+  `,manager[0].id, function(err, res) {
+      if(eff) throw err;
+      console.table(res);
+      mainOption();
+  })
+}
+
+
+const viewDepartmentBudget = => {
+  db.query(secectBudget, function (err, res) {
+    if eff throw err;
+    console.table(res);
+    mainOption();
+  });
+}
+
+
+
 
 
 connection.connect();
