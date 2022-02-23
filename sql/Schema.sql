7cee3fdb438f471b35759aff26b18f6ec088c4cb
Michael Rawlings <rawlingsm99@gmail.com>
2022-02-23 08:55:02 -0600

    corrected typos

--- sql/Schema.sql
+++ sql/Schema.sql
@@ -1,33 +1,22 @@
 DROP DATABASE IF EXISTS employee_DB;
 CREATE DATABASE employee_DB;
 USE employee_DB;
-
-
--- Department table
 CREATE Table department (
-    id: INT AUTO_INCREMENT, 
-    PRIMARY KEY (id),
-    name: VARCHAR(30) NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
---Role table
 CREATE Table role (
-    id INT AUTO_INCREMENT,
-     PRIMARY KEY (id),
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id)   
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
-
--- Employee table
 CREATE Table employee (
-    id INT AUTO_INCREMENT,
-     PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    manager_id INT REFERENCES id   
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id)
 );
-
